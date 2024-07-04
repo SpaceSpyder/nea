559964cf2537d14ba99c246668c260ab63ccb769
Alex <aijdavis2006@gmail.com>
2024-07-04 19:10:28 +0100

    adding files to repository

---
+++ app.py
@@ -0,0 +1,64 @@
+#pip install -U Flask
+#pip install Jinja2
+
+import sqlite3
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__, static_folder='templates', static_url_path='')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        return f'You entered: {user_input}'
+    else:
+        return 'ERROR Method Not Allowed'
+
+@app.route('/testGame')
+def testGame():
+    return render_template('game.html')
+
+@app.route('/testGame2')
+def testGame2():
+    return render_template('game2.html')
+
+@app.route('/temp')
+def temp():
+    return render_template('temp.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUp.html')
+
+@app.route('/dbTest')
+def dbTest():
+    conn = sqlite3.connect('databases/database.db')
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users')
+
+    # Fetch all rows from the executed query
+    rows = cursor.fetchall()
+
+    # Iterate over the rows and print each one
+    result = ""
+    for row in rows:
+        result+=", " + str(row)
+
+    conn.close()
+
+    return render_template('record.html', name=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
